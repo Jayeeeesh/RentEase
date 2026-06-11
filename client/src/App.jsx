1c7a74ac8d4cbd133d3d44cf2a7c4c81a37e1ed9
Jayeeeesh <jayeshthakur132@gmail.com>
2026-06-11 18:30:56 +0530

    feat: implement MainLayout component to encapsulate Navbar and main content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import Navbar from './components/shared/NavBar'
+import MainLayout from './layouts/MainLayout'
 
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
+    <MainLayout>
       <Routes>
         <Route path="/" element={<div>RentEase Home</div>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+      </MainLayout>
     </BrowserRouter>
   )
 }
