54b2e87edb8e8bd7495449957354295e3f976412
Jayeeeesh <jayeshthakur132@gmail.com>
2026-06-14 11:55:42 +0530

    refactor: clean up import statements in useAuth hook

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,19 +1,9 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { useCallback } from 'react'
 
-import {
-  authStart,
-  authSuccess,
-  authFailure,
-  logout,
-  clearError,
-} from '../features/auth/authSlice'
+import { authStart, authSuccess, authFailure, logout, clearError, } from '../features/auth/authSlice'
 
-import {
-  loginAPI,
-  registerAPI,
-  logoutAPI,
-} from '../features/auth/authAPI'
+import { loginAPI, registerAPI, logoutAPI } from '../features/auth/authAPI'
 
 const useAuth = () => {
   const dispatch = useDispatch()
